026417035f4c6b58dc3ef26600bfa0999c3e3e9d
tovejungenfelt <65162787+tovejungenfelt@users.noreply.github.com>
2021-12-02 13:31:15 +0100

    dec2 solution

--- dec2.py
+++ dec2.py
@@ -6,9 +6,36 @@ import fileinput
 import itertools
 
 
-depths = []
-with fileinput.input(files=('input2.txt')) as f:
-    for line in f:
-        depths.append(int(line))
+data = pd.read_csv('input2.txt', sep=' ', names=['direction','units'])
+dlist = data.values.tolist()
 
-print(depths)
+# PART I
+pos = 0
+depth = 0
+
+for i in range(0, len(dlist)):
+    if dlist[i][0] == "forward":
+        pos += dlist[i][1]
+    elif dlist[i][0] == "down":
+        depth += dlist[i][1]
+    else:
+        depth -= dlist[i][1]
+
+print("What do you get if you multiply your final horizontal position by your final depth? ", depth*pos)
+
+# PART II
+
+pos = 0
+depth = 0
+aim = 0
+
+for j in range(0, len(dlist)):
+    if dlist[j][0] == "forward":
+        pos += dlist[j][1]
+        depth += aim*dlist[j][1]
+    elif dlist[j][0] == "down":
+        aim += dlist[j][1]
+    else:
+        aim -= dlist[j][1]
+
+print("What do you get if you multiply your final horizontal position by your final depth? ", depth*pos)
