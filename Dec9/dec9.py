df1b931bee46443887406af5d35b7adb666e2334
tovejungenfelt <65162787+tovejungenfelt@users.noreply.github.com>
2021-12-09 13:17:07 +0100

    part 1 day 9

--- Dec9/dec9.py
+++ Dec9/dec9.py
@@ -4,3 +4,50 @@ lines = []
 with fileinput.input(files=('input.txt')) as f:
     for line in f:
         lines.append(line.strip())
+
+height = []
+for i in lines:
+    height.append([int(x) for x in i ])
+
+marked = [[False]*len(height[0]) for elem in height[0]]
+final = 0
+
+for i in range(0,len(height)):
+    for j in range(len(height[0])):
+        if i == 0:
+            if j == 0:
+                n = [height[i][j + 1], height[i + 1][j]]
+            elif j == len(height[0])-1:
+                n = [height[i][j - 1], height[i + 1][j]]
+            else:
+                n = [height[i][j + 1], height[i][j - 1], height[i + 1][j]]
+        elif j == 0:
+            if i == len(height)-1:
+                n = [height[i][j + 1], height[i - 1][j]]
+            else:
+                n = [height[i][j + 1], height[i - 1][j], height[i + 1][j]]
+        elif i == len(height)-1:
+            if j == len(height[0])-1:
+                n = [height[i][j - 1], height[i - 1][j]]
+            else:
+                n = [height[i][j + 1], height[i][j - 1], height[i - 1][j]]
+        elif j == len(height[0])-1:
+            n = [height[i + 1][j], height[i][j - 1], height[i - 1][j]]
+        else:
+            n = [height[i + 1][j], height[i][j - 1], height[i - 1][j], height[i][j + 1]]
+        bool = []
+        for num in n:
+            if height[i][j] < num:
+                bool.append(True)
+            else:
+                bool.append(False)
+        if (sum(bool) == len(bool)):
+            final += height[i][j] + 1
+
+print(final)
+
+
+
+
+
+
